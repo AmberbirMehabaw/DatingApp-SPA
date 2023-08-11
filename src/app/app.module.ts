06edb6f15897674986a2dfa6737017c328c4fba8
Amberbir Mehabaw <Amberbir2014@outlook.com>
2023-08-11 17:54:32 +0300

    module 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorInterceptor, ErrorInterceptorProvide } from './services/error-interceptor';
 
 
 @NgModule({
@@ -20,13 +22,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
-   
   ],
-  providers: [],
+  providers: [ErrorInterceptorProvide],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
